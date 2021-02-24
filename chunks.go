a9633174585442ab0b64a313d0fdb5397260a722
Leo Gaskin <leo.gaskin@le0.gs>
2021-02-25 00:44:10 +0100

    Remove Chunks and rename previous SingleChunks functions
    The previous Chunks implementation was not that useful and could
    easily result in broken output even when used correctly.  Users, for
    now, should manually split chunks at reasonable semantic boundaries.
    
    Also remove associated test case.

--- chunks.go
+++ chunks.go
@@ -1,15 +1,11 @@
 package mobi
 
-import "github.com/leotaku/mobi/records"
-
-// SingleChunks produces a list of chunks from several strings.  In
-// the resulting list of chunks, each chunk exactly corresponds the
-// given string with the same index.
+// Chunks produces a list of chunks from one or more strings.
 //
-// This is useful in cases where the size of a chunk has influence on
-// the presentation of the generated book.  In the particular case of
-// fixed-layout books, a single chunk also represents a single page.
-func SingleChunks(ss ...string) []Chunk {
+// In the resulting list of chunks, each chunk exactly corresponds the
+// given string with the same index.  In almost all cases, this is the
+// preferred method of converting KF8 HTML into a list of chunks.
+func Chunks(ss ...string) []Chunk {
 	result := make([]Chunk, 0)
 	for _, s := range ss {
 		result = append(result, Chunk{
@@ -17,26 +13,4 @@ func SingleChunks(ss ...string) []Chunk {
 		})
 	}
 	return result
-}
-
-// Chunks splits a string into a list of chunks sized at most 4096
-// bytes.  In most cases, this is the preferred method of converting
-// book text into a list of chunks.
-//
-// Using this method, chunks should be very roughly aligned to the
-// maximum text record size, which may or may not be beneficial for
-// performance.
-func Chunks(s string) []Chunk {
-	result := make([]Chunk, 0)
-	for len(s) > records.TextRecordMaxSize {
-		body := s[:records.TextRecordMaxSize]
-		s = s[records.TextRecordMaxSize:]
-		result = append(result, Chunk{
-			Body: body,
-		})
-	}
-
-	return append(result, Chunk{
-		Body: s,
-	})
 }
